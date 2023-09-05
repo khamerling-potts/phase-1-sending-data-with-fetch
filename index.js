997f77264c70beb3c600e10630958d35169b7b91
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-09-05 14:57:55 -0400

    sending data fetch lab done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData(name, email) {
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  };
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then((response) => response.json())
+    .then((data) => handleResponse(data))
+    .catch((error) => {
+      document.querySelector("body").innerHTML = error.message;
+    });
+}
+
+function handleResponse(user) {
+  console.log(user);
+  document.querySelector("body").innerHTML = user.id;
+}
